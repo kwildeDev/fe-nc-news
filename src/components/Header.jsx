20bcc08b0373a7eb17c3de8781270171183a8015
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2024-12-29 22:52:01 +0000

    toggle between light and dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
-import { AppBar, Box } from '@mui/material';
+import { AppBar } from '@mui/material';
 import Nav from './Nav';
 
 
-const Header = () => {
+const Header = ({ onThemeToggle, isDarkMode }) => {
 
     return (
         <>
         <AppBar
             position="sticky"
-            elevation={0}
+            //elevation={0}
+            color='primary'
             sx={{top: 0,
-                borderTop: '8px solid white',
-            }}>
-            <Nav/>
+                borderTop: (theme) => `8px solid $(theme.palette.primary.main)`,
+            }}>     
+            <Nav onThemeToggle={onThemeToggle} isDarkMode={isDarkMode}/>
         </AppBar>
         </>
     )
